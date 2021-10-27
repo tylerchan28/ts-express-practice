eb17efedb3ad39158575dedfec5797209bd8a45d
Tyler Chan <tylerchan28@gmail.com>
2021-10-27 12:34:47 -0700

    Remove node_modules

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { NextFunction } from "express";
+import { json } from "body-parser";
+
+import todoRoutes from "./routes/todos";
+const app = express();
+
+app.use(json());
+
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: express.Request, res: express.Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message })
+})
+
+app.listen(3000)
